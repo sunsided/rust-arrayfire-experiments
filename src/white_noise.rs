e517c2c8d3e5340fa827c1546710ed72d437d85f
Markus Mayer <widemeadows@gmail.com>
2020-09-04 20:47:53 +0200

    Use CPU back-end for white noise example

--- src/white_noise.rs
+++ src/white_noise.rs
@@ -1,12 +1,13 @@
 use arrayfire::*;
 
 fn main() {
+    set_backend(Backend::CPU);
     set_device(0);
     info();
 
     let wnd = Window::new(1280, 720, String::from("White Noise"));
 
-    // TODO: Need to clarify channel ordering. On my NVIDIA GPU, order is HWCN, but the original code had WHCN.
+    // Channel order appears to be HWCN regardless of the backend.
     let dims = Dim4::new(&[720, 1280, 1, 1]);
 
     loop {
