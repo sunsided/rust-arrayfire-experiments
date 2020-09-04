a6908f85db8556d3eaa9a10183d1d83b321d41bd
Markus Mayer <widemeadows@gmail.com>
2020-09-04 17:11:25 +0200

    Add device info to trivial.rs

--- src/trivial.rs
+++ src/trivial.rs
@@ -1,9 +1,13 @@
 use arrayfire::*;
 
 fn main() {
-    let num_rows: u64 = 5;
-    let num_cols: u64 = 3;
-    let dims = Dim4::new(&[num_rows, num_cols, 1, 1]);
+    const NUM_ROWS: u64 = 5;
+    const NUM_COLS: u64 = 3;
+
+    let dims = Dim4::new(&[NUM_ROWS, NUM_COLS, 1, 1]);
     let a = randu::<f32>(dims);
+
     af_print!("Create a 5-by-3 matrix of random floats on the GPU", a);
+
+    info();
 }
